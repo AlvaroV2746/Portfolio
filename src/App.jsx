b3a2e36133979585a93cd51aff1d6e0e4a938a0d
Alvaro Villarreal <alvarovillarreal2746@hotmail.com>
2026-08-19 01:29:46 -0500

    navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ function App() {
           <Navbar />
         </header>
         <Routes>
-          <Route path="/Portfolio/home" element={<Home />} />
-          <Route path="/Portfolio/about" element={<About />} />
-          <Route path="/Portfolio/projects" element={<Projects />} />
-          <Route path="/Portfolio/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
