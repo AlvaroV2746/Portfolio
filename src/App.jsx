e79636d8329a66e9a60dd54c8a2b0e8a33488a24
Alvaro Villarreal <alvarovillarreal2746@hotmail.com>
2026-08-19 02:05:11 -0500

    routes changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,7 +9,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename="/Portfolio">
+    <Router>
       <div className="d-flex flex-column h-100">
         <header>
           <Navbar />
